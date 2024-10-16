b57da7882f15270eb28bf5525cb09bbde119e70f
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-10-16 19:14:38 +0530

    dbconnectoin.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,12 +1,12 @@
 import mysql from 'mysql2'
-import { DB_HOST,DB_USER, DB_PASSWORD, DB_NAME,DB_PORT } from './envConfig';
+import { DB_HOST,DB_USER, DB_PASSWORD, DB_NAME,DB_PORT } from './envConfig.js';
 
 const pool = mysql.createPool({
     host: DB_HOST,  // Host from your MySQL server
     user: DB_USER,                                        // MySQL username
     password: DB_PASSWORD,                    // MySQL password
     database: DB_NAME,                                   // Database name
-    port: 22466,                                             // Custom port
+    port: DB_PORT,                                             // Custom port
     waitForConnections: true,                                // Handle multiple requests
     connectionLimit: 10,                                     // Pool size
     queueLimit: 0,                                           // No limit on queued requests
@@ -15,25 +15,4 @@ const pool = mysql.createPool({
     }
 });
 
-// export default pool.promise();
-
-const pool = new Pool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-});
-
-const connectDB = async()=>{
-  try {
-    await pool.connect();
-    console.log("Database Connection Sucessfull");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-connectDB();
-
-export default pool;
+export default pool.promise();
