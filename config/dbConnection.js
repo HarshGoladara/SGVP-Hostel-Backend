6102b469c05d08059f7a535b435cbaa34213edae
Vasu Parsaniya <vasuparsaniya21@gmail.com>
2024-12-15 20:51:30 +0530

    pull and push

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,3 +1,45 @@
+// import postgres from 'pg';
+// import pkg from 'pg';
+// import {
+//   DB_HOST,
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_NAME,
+//   DB_PORT,
+// } from './envConfig.js';
+// import { DATABASE_URL } from './envConfig.js';
+
+// const { Pool } = pkg;
+
+// // local Postgres connection
+// const localPool = new postgres.Pool({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+//   port: DB_PORT || 5432,
+//   max: 10,
+// });
+
+// // Cloud Postgres connection
+// const cloudPool = new Pool({
+//   connectionString: DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false, // For secure connections
+//   },
+// });
+
+// const pool = DATABASE_URL ? cloudPool : localPool;
+// if (DATABASE_URL) {
+//   console.log('Cloud connection');
+// } else {
+//   console.log('local connection');
+// }
+
+// export default pool;
+
+
+
 import postgres from 'pg';
 import pkg from 'pg';
 import {
@@ -7,7 +49,9 @@ import {
   DB_NAME,
   DB_PORT,
 } from './envConfig.js';
-import { DATABASE_URL } from './envConfig.js';
+import {
+  DATABASE_URL,
+} from './envConfig.js';
 
 const { Pool } = pkg;
 
@@ -30,10 +74,11 @@ const cloudPool = new Pool({
 });
 
 const pool = DATABASE_URL ? cloudPool : localPool;
-if (DATABASE_URL) {
-  console.log('Cloud connection');
-} else {
-  console.log('local connection');
+if(DATABASE_URL){
+  console.log("Cloud connection");
+}
+else{
+  console.log("local connection");
 }
 
 export default pool;
--- config/envConfig.js
+++ config/envConfig.js
@@ -1,18 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
+// export const {
+//   PORT,
+//   DB_HOST,
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_NAME,
+//   DB_PORT,
+//   DATABASE_URL,
+//   PGHOST,
+//   PGUSER,
+//   PGDATABASE,
+//   PGPASSWORD,
+//   TWILIO_ACCOUNT_SID,
+//   TWILIO_AUTH_TOKEN,
+//   TWILIO_PHONE_NUMBER,
+// } = process.env;
+
 export const {
-  PORT,
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_PORT,
-  DATABASE_URL,
-  PGHOST,
-  PGUSER,
-  PGDATABASE,
-  PGPASSWORD,
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN,
-  TWILIO_PHONE_NUMBER,
+    PORT,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT,
+    DATABASE_URL,
+    PGHOST,
+    PGUSER,
+    PGDATABASE,
+    PGPASSWORD
 } = process.env;
